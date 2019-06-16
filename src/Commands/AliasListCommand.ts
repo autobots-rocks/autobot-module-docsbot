8b317b2b23a95036b5a967841ad74f3e53e2f0b9
Matthew Davis <matthew@matthewdavis.io>
2019-06-16 17:32:38 -0500

    bump

--- src/Commands/AliasListCommand.ts
+++ src/Commands/AliasListCommand.ts
@@ -51,18 +51,18 @@ export class AliasListCommand extends CommandBase {
 
             console.log(aliases);
 
-            const embed = new RichEmbed().setTitle('docsbot alias add')
-                                         .setColor(3447003)
-                                         .setDescription(`the following language aliases are availble:`)
-                                         .setFooter('https://github.com/autobots-rocks/autobot-docsbot');
-
-            // for (let key in aliases) {
-            //             //
-            //             //     embed.addField(key, aliases[ key ], true);
-            //             //
-            //             // }
-
-            command.obj.channel.send(embed);
+            // const embed = new RichEmbed().setTitle('docsbot alias add')
+            //                              .setColor(3447003)
+            //                              .setDescription(`the following language aliases are availble:`)
+            //                              .setFooter('https://github.com/autobots-rocks/autobot-docsbot');
+            //
+            // // for (let key in aliases) {
+            // //             //
+            // //             //     embed.addField(key, aliases[ key ], true);
+            // //             //
+            // //             // }
+            //
+            // command.obj.channel.send(embed);
 
         } else {
 
