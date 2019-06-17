81428104b29c04b6cc8f0ad2782cbc7928afbdfe
Matthew Davis <matthew@matthewdavis.io>
2019-06-16 23:57:46 -0500

    bump

--- src/Commands/HelpCommand.ts
+++ src/Commands/HelpCommand.ts
@@ -1,7 +1,8 @@
-import { Command, CommandBase, CommandParser, Event } from '@autobot/common';
-import { RichEmbed }                                  from 'discord.js';
-import * as fs                                        from 'fs';
-import { ListFilesCommand }                           from './ListFilesCommand';
+import { Command, CommandBase, CommandParser, Config, Event } from '@autobot/common';
+import { RichEmbed }                                          from 'discord.js';
+import * as fs                                                from 'fs';
+import { Alias }                                              from '../_lib/Alias';
+import { ListFilesCommand }                                   from './ListFilesCommand';
 
 /**
  * Outputs help information.
@@ -17,7 +18,7 @@ export class HelpCommand extends CommandBase {
         super({
 
             event: Event.MESSAGE,
-            name: `${ process.env.DOCSBOT_PREFIX_HELP }`,
+            name: process.env.DOCSBOT_PREFIX_HELP,
             group: 'docs',
             requiredEnvVars: [
 
@@ -53,17 +54,29 @@ export class HelpCommand extends CommandBase {
 
         if (result) {
 
-            command.obj.channel.send(new RichEmbed().setTitle(`docsbot help`)
-                                                    .setDescription(`docsbot is a bot that searches devdocs.io and cheat.sh`)
-                                                    .setColor(3447003)
-                                                    .addField('searching', `use \`${ process.env.DOCSBOT_PREFIX_SEARCH }<language> <method>\` such as \`${ process.env.DOCSBOT_PREFIX_SEARCH }javascript async\``)
-                                                    .addField('seeing what terms there are', `use \`${ process.env.DOCSBOT_PREFIX_TERMS } <language>\` such as \`${ process.env.DOCSBOT_PREFIX_TERMS } javascript\`. this will output a list of all searchable terms.`)
-                                                    .addField('seeing what aliases there are', `use \`${ process.env.DOCSBOT_PREFIX_ALIAS_LIST }\`. this will output a list of all aliases.`)
-                                                    .addField('searchable languages list', result.filter(val => ListFilesCommand.BLOCKED_FILES.indexOf(val) === -1).join(', ').replace(/\.json/g, ''))
-                                                    .addField('downloading new languages', `use \`${ process.env.DOCSBOT_PREFIX_UPDATE } <language>\` such as \`${ process.env.DOCSBOT_PREFIX_UPDATE } javascript\``)
-                                                    .addField('searching shell commands', `use \`${ process.env.DOCSBOT_PREFIX_CHEAT } <command>\` such as \`${ process.env.DOCSBOT_PREFIX_CHEAT } grep\`. this will use https://cheat.sh to search for a shell command.`)
-                                                    .setURL('https://github.com/autobots-rocks/autobot-docsbot')
-                                                    .setFooter('https://github.com/autobots-rocks/autobot-docsbot'));
+            const embed = new RichEmbed().setTitle(`docsbot help`)
+                                         .setDescription(`docsbot is a bot that searches devdocs.io and cheat.sh`)
+                                         .setColor(3447003)
+                                         .addField('searching', `use \`${ process.env.DOCSBOT_PREFIX_SEARCH }<language> <method>\` such as \`${ process.env.DOCSBOT_PREFIX_SEARCH }javascript async\``)
+                                         .addField('seeing what terms there are', `use \`${ process.env.DOCSBOT_PREFIX_TERMS } <language>\` such as \`${ process.env.DOCSBOT_PREFIX_TERMS } javascript\`. this will output a list of all searchable terms.`)
+                                         .addField('seeing what aliases there are', `use \`${ process.env.DOCSBOT_PREFIX_ALIAS_LIST }\`. this will output a list of all aliases.`);
+
+
+            const aliases = Config.load<Alias>(process.env.DOCSBOT_ALIASES_CONFIG_PATH);
+
+            for (let key in aliases) {
+
+                embed.addField(key, aliases[ key ], true);
+
+            }
+
+            embed.addField('searchable languages list', result.filter(val => ListFilesCommand.BLOCKED_FILES.indexOf(val) === -1).join(', ').replace(/\.json/g, ''))
+                 .addField('downloading new languages', `use \`${ process.env.DOCSBOT_PREFIX_UPDATE } <language>\` such as \`${ process.env.DOCSBOT_PREFIX_UPDATE } javascript\``)
+                 .addField('searching shell commands', `use \`${ process.env.DOCSBOT_PREFIX_CHEAT } <command>\` such as \`${ process.env.DOCSBOT_PREFIX_CHEAT } grep\`. this will use https://cheat.sh to search for a shell command.`)
+                 .setURL('https://github.com/autobots-rocks/autobot-docsbot')
+                 .setFooter('https://github.com/autobots-rocks/autobot-docsbot');
+
+            command.obj.channel.send(embed);
 
         } else {
 
